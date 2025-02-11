7823c4daa37d022f7f6521fda1bb259ded8f0d06
gabryelns <gabryelnick91@gmail.com>
2025-02-10 23:20:10 -0300

    adiciona 64 e 1751

---
+++ 1751_Maximum_Number_of_Events_That_Can_Be_Attended_II.py
@@ -0,0 +1,32 @@
+from bisect import bisect_right
+
+class Solution(object):
+
+    @staticmethod
+    def maxValue(events, k):
+        # Ordena os eventos pelo dia de término
+        events.sort(key=lambda x: x[1])
+    
+        # Extrai os dias de término para facilitar a busca binária
+        end_days = [event[1] for event in events]
+    
+        # Inicializa a tabela de programação dinâmica
+        # dp[i][j] representa o valor máximo que pode ser obtido com os primeiros i eventos e j seleções
+        n = len(events)
+        dp = [[0] * (k + 1) for _ in range(n + 1)]
+    
+        for i in range(1, n + 1):
+            start, end, value = events[i-1]
+        
+            # Encontra o último evento que não conflita com o evento atual
+            last_non_conflict = bisect_right(end_days, start - 1, 0, i-1)
+        
+            for j in range(1, k + 1):
+                # Escolha 1: Não selecionar o evento atual
+                dp[i][j] = dp[i-1][j]
+            
+                # Escolha 2: Selecionar o evento atual e adicionar ao valor máximo obtido até o último evento não conflitante
+                dp[i][j] = max(dp[i][j], dp[last_non_conflict][j-1] + value)
+    
+        # O valor máximo será o maior valor na última linha da tabela dp
+        return dp[n][k]
---
+++ 64_Minimum_Path_Sum.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def minPathSum(self, grid):
+        
+        m = len(grid)  # Número de linhas na grade
+        n = len(grid[0])  # Número de colunas na grade
+    
+        # Inicializa o array de distâncias
+        # dist[i][j] armazenará a soma mínima do caminho de (0, 0) até (i, j)
+        dist = [[float('inf')] * n for _ in range(m)]
+        dist[0][0] = grid[0][0]  # A soma mínima para o ponto de partida é o valor da célula (0, 0)
+    
+        # Relaxamento das arestas utilizando o belman ford para a direita e para baixo em cada célula para encontrar o caminho mais curto.
+        for i in range(m):
+            for j in range(n):
+                # Relaxamento para a célula abaixo (i+1, j)
+                if i < m - 1:  # Verifica se a célula abaixo existe
+                    dist[i+1][j] = min(dist[i+1][j], dist[i][j] + grid[i+1][j])
+                    # Atualiza a distância mínima para a célula abaixo, se um caminho menor for encontrado
+                
+                # Relaxamento para a célula à direita (i, j+1)
+                if j < n - 1:  # Verifica se a célula à direita existe
+                    dist[i][j+1] = min(dist[i][j+1], dist[i][j] + grid[i][j+1])
+                    # Atualiza a distância mínima para a célula à direita, se um caminho menor for encontrado
+    
+        # Retorna a soma mínima do caminho até a célula (m-1, n-1)
+        return dist[m-1][n-1]
